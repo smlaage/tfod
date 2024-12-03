ce5549df4c70252f265351a52b746bff614dae7a
Stephan Laage-Witt <50277662+smlaage@users.noreply.github.com>
2024-12-03 09:37:27 +0100

    Update resize_images.py

--- resize_images.py
+++ resize_images.py
@@ -14,23 +14,33 @@ SLW - 08-2023
 import os
 import cv2
 
-img_dir = "images"
-org_dir = "original"
-file_list = os.listdir(org_dir)
+# Directories
+project_dir = "."
+source_image_dir = "original"
+source_image_path = os.path.join(project_dir, source_image_dir)
+dest_image_dir = "images"
+dest_image_path = os.path.join(project_dir, dest_image_dir)
+
+print("Resizing images from folder '" + source_image_path + "' to folder '" + dest_image_path + "'") 
+
+# Read source data
+file_list = os.listdir(source_image_path)
 cnt = 0
 
-print("Resizing images from folder '" + org_dir + "' to '" + img_dir + "'") 
-
+# Resize images one by one
 for file in file_list:
     if file[-4:] in (".jpg", ".png"):
         print(cnt, end='\r')
-        img = cv2.imread(os.path.join(org_dir, file))
+        img = cv2.imread(os.path.join(source_image_path, file))
         height, width = img.shape[:2]
         if width > 1024:
             img = cv2.resize(img, (1024, height * 1024 // width))
-        cv2.imwrite(os.path.join(img_dir, file), img)
+        elif height > 1024:
+            img = cv2.resize(img, (width * 1024 // height), 1024)
+        cv2.imwrite(os.path.join(dest_image_path, file), img)
         print(file, "processed", end='\r')
         cnt += 1
     
 print(cnt, "images processed")
 print("Done")
+
